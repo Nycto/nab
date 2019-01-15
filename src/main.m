21af5039f004c5ccfcc37bb4c6071dff4467c15a
Nycto <James@RoundEights.com>
2019-01-15 10:25:04 -0800

    Use main.m instead of main.c

---
+++ src/main.m
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import "backend.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        NimMain();
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
