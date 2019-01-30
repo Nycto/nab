36c5549a5e49d31c6d1a95288a75eec59990ef2f
Nycto <James@RoundEights.com>
2019-01-30 08:37:42 -0800

    Mac compiler flag organization

--- nom/src/private/mac.nim
+++ nom/src/private/mac.nim
@@ -1,13 +1,42 @@
-import config, os
+import config, os, util, pegs
+
+type MacSdk = enum
+    ## Mac SDKs that can be targeted for compilation
+    iPhone,
+    iPhoneSim
+
+proc dirName(sdk: MacSdk): string =
+    ## Given an SDK, returns the file system name used for it
+    case sdk
+    of MacSdk.iPhone: "iPhoneOS"
+    of MacSdk.iPhoneSim: "iPhoneSimulator"
 
 proc xCodeAppPath*(self: Config): string =
     ## Where to find XCode
     "/Applications/Xcode.app"
 
-proc iOsSimulatorSdkPath*(self: Config): string =
-    ## The file path for the ios simulator SDK
-    let filename = "iPhoneSimulator" & self.iOsSimSdkVersion & ".sdk"
-    result = self.xCodeAppPath / "Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs" / filename
+proc xCodeSdksPath(self: Config, sdk: MacSdk): string =
+    ## Returns the directory name for an SDK. This path contains all the versions for that SDK
+    result = self.xCodeAppPath / "Contents/Developer/Platforms/" & sdk.dirName & ".platform/Developer/SDKs"
+    discard result.requireDir
+
+proc sdkVersion(self: Config, sdk: MacSdk): string =
+    ## Returns the highest installed SDK version
+    let searchDir = self.xCodeSdksPath(sdk)
+
+    for file in walkDir(searchDir):
+        var dirName = splitFile(file.path).name
+        var matches = dirName.findAll(peg"\d+\.\d+")
+        if matches.len() > 0:
+            return matches[matches.len() - 1]
+
+    raise newException(OSError, "Could not find any SDKs. Searched in " & searchDir)
+
+proc sdkPath*(self: Config, sdk: MacSdk): string =
+    ## The file path for a specific SDK
+    let filename = sdk.dirName & self.sdkVersion(MacSdk.iPhoneSim) & ".sdk"
+    result = self.xCodeSdksPath(sdk) / filename
+    discard result.requireFile
 
 proc iOsSimFlags*(self: Config): seq[string] =
     ## Compiler flags for compiling for the ios simulator
@@ -15,5 +44,5 @@ proc iOsSimFlags*(self: Config): seq[string] =
         "--cpu:amd64",
         "-d:ios", "-d:simulator",
         "--passC:-isysroot", "--passL:-isysroot",
-        "--passC:" & self.iOsSimulatorSdkPath, "--passL:" & self.iOsSimulatorSdkPath,
+        "--passC:" & self.sdkPath(MacSdk.iPhoneSim), "--passL:" & self.sdkPath(MacSdk.iPhoneSim),
         "--passL:-fobjc-link-runtime" ]
--- nom/src/private/util.nim
+++ nom/src/private/util.nim
@@ -9,6 +9,18 @@ proc ensureParentDir*(path: string) =
     ## Guarantees a parent directory exists
     path.parentDir.ensureDir
 
+proc requireDir*(path: string): string =
+    ## Requires that a directory exists or throws
+    result = path
+    if not path.dirExists:
+        raise newException(OSError, "Directory does not exist: " & path)
+
+proc requireFile*(path: string): string =
+    ## Requires that a file exists or throws
+    result = path
+    if not path.fileExists:
+        raise newException(OSError, "File does not exist: " & path)
+
 template isEmpty*(iter: untyped): bool =
     ## Whether an iterator is empty
     var result: bool = true
