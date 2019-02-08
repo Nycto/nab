8ebbb4f16b7e69fd7bd4f1282f488917927f5788
Nycto <James@RoundEights.com>
2019-02-08 14:22:24 -0800

    Re-implement rectangles.c

--- examples/sdl2/src/sdl2example.nim
+++ examples/sdl2/src/sdl2example.nim
@@ -1,28 +1,70 @@
 import sdl2, random
 
-discard sdl2.init(INIT_EVERYTHING)
+proc render(renderer: RendererPtr) =
+    var renderW, renderH: cint
+    renderer.getLogicalSize(renderW, renderH)
 
-var
-  window: WindowPtr
-  render: RendererPtr
+    # Come up with a random rectangle
+    var rectangle = rect(
+        x = cint(rand(renderW)),
+        y = cint(rand(renderH)),
+        w = cint(rand(64) + 64),
+        h = cint(rand(64) + 64))
 
-window = createWindow("SDL Skeleton", 100, 100, 640,480, SDL_WINDOW_SHOWN)
-render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+    # Come up with a random color
+    let r = uint8(rand(205) + 50)
+    let g = uint8(rand(205) + 50)
+    let b = uint8(rand(205) + 50)
 
-var
-  evt = sdl2.defaultEvent
-  runGame = true
+    # Fill the rectangle in the color
+    renderer.setDrawColor(r, g, b, 255)
+    renderer.fillRect(rectangle)
 
-while runGame:
-  while pollEvent(evt):
-    if evt.kind == QuitEvent:
-      runGame = false
-      break
+    # update screen
+    renderer.present
 
-  render.setDrawColor uint8(rand(255)), 0, 0, 255
-  render.clear
+template sdl2assert(condition: SDL_Return, message: typed): untyped =
+    if condition == SDL_Return.SdlError:
+        raise newException(AssertionError, message & "; " & $getError())
 
-  render.present
+template sdl2assert(condition: cint, message: typed): untyped =
+    if condition != 0:
+        raise newException(AssertionError, message & "; " & $getError())
 
-destroy render
-destroy window
+proc main() =
+
+    # initialize SDL
+    sdl2assert(sdl2.init(INIT_VIDEO), "Failed to initialize SDL")
+
+    # seed random number generator
+    randomize()
+
+    # create window and renderer */
+    let window = createWindow(nil, 0, 0, 320, 480, SDL_WINDOW_ALLOW_HIGHDPI)
+    let renderer = createRenderer(window, -1, 0)
+
+    defer: sdl2.quit()
+
+    let (windowW, windowH) = window.getSize()
+    sdl2assert(renderer.setLogicalSize(windowW, windowH), "Unable to set logical size")
+
+    # Fill screen with black
+    renderer.setDrawColor(0, 0, 0, 255)
+    renderer.clear()
+
+    # Enter render loop, waiting for user to quit
+    var done = false
+    while not done:
+        var event: Event
+        while (pollEvent(event)):
+            if event.kind in { QuitEvent, KeyDown, FingerDown, MouseButtonDown }:
+                done = true
+        renderer.render()
+        delay(1)
+
+try:
+    main()
+except:
+    echo "ERROR: ", getCurrentExceptionMsg()
+    discard showSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "INTERNAL ERROR", getCurrentExceptionMsg(), nil)
+
